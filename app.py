4fa7ddf03e2c67e50f0ec6bbdce5de928d576538
bednie <88800117+bednie@users.noreply.github.com>
2023-04-27 19:55:56 +0000

    Add simple-websocket

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_socketio import SocketIO
 from flask_socketio import join_room, leave_room, send
 import random
 from string import ascii_uppercase
+import os
 
 # Create app
 app = Flask(__name__)
