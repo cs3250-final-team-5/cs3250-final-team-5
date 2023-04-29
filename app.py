36a1409b0618ab44efe7ce077027b9e6aed16101
bednie <88800117+bednie@users.noreply.github.com>
2023-04-29 14:44:15 +0000

    Update Procfile

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def disconnect():
 
 # Run app
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
